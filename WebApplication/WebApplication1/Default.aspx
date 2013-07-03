9f87f4328d76e7c1481fcc5fb7b17424f5da41a9
Sagnik Sinha Roy <sagnik.sinharoy@gmail.com>
2013-07-03 12:14:17 +0530

    first commit

---
+++ WebApplication/WebApplication1/Default.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeBehind="Default.aspx.cs" Inherits="WebApplication1._Default" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <form id="form1" action="javascript:void(0)">
+        <h1>Combinations Generator</h1><br />
+        <asp:TextBox ID="input" runat="server"></asp:TextBox>
+        <br /><br />
+        <asp:button id="clickMeButton" runat="server" text="Generate" onClick="clickMeButton_Click"/> 
+        <asp:button id="Button2" runat="server" text="Clear" OnClick="Button2_Click"/>
+        <br /><br />
+        <asp:TextBox ID="output" style="resize:none" runat="server" TextMode="multiline" Width="200px" Height="100px" ReadOnly="true"></asp:TextBox>
+    </form>
+</asp:Content>
---
+++ WebApplication/WebApplication1/Default.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void clickMeButton_Click(object sender, EventArgs e)
+        {
+            List<string> list = input.Text.Split(',').ToList<string>();
+            int i = 1;
+            output.Text = "";
+            foreach (var sequence in list.AllCombinations())
+            {
+                if (string.Join(" ", sequence) != "")
+                {
+                    if (i == 1)
+                    {
+                        output.Text += (i.ToString() + ")" + " " + string.Join(" ", sequence));
+                        i++;
+                    }
+                    else
+                    {
+                        output.Text += (Environment.NewLine + i.ToString() + ")" + " " + string.Join(" ", sequence));
+                        i++;
+                    }
+                }
+            }
+        }
+
+        public void Button2_Click(object sender, EventArgs e)
+        {
+            input.Text = "";
+            output.Text = "";
+        }
+    }
+    public static class a
+    {
+        public static IEnumerable<IEnumerable<T>> Combinations<T>(this IEnumerable<T> source, int n)
+        {
+            if (n == 0)
+                yield return Enumerable.Empty<T>();
+
+
+            int count = 1;
+            foreach (T item in source)
+            {
+                foreach (var innerSequence in source.Skip(count).Combinations(n - 1)
+)
+                {
+                    yield return new T[] { item }.Concat(innerSequence);
+                }
+                count++;
+            }
+        }
+        public static IEnumerable<IEnumerable<T>> AllCombinations<T>(this IList<T> source)
+        {
+            IEnumerable<IEnumerable<T>> output = Enumerable.Empty<IEnumerable<T>>();
+            for (int i = 0; i < source.Count + 1; i++)
+            {
+                output = output.Concat(source.Combinations(i));
+            }
+            return output;
+        }
+    }
+}
